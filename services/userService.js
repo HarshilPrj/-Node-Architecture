cada023a61e859806e7b1ec34bc872affb7563a2
harshil <harshil.prajapati@coodeit.com>
2022-09-25 21:55:21 +0530

    done

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,9 @@
 const userModel = require("../model/userModel");
 
 const getdata = async () => {
-  return await userModel.query();
+  return await userModel
+    .query()
+    .where("users_email", "harshil.prajapati@coodeit.com");
 };
 
 module.exports = { getdata };
