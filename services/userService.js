3c8611fc91a528136f6e0963be473265735c7c31
harshil <harshil.prajapati@coodeit.com>
2022-09-26 22:26:11 +0530

    done login  api

--- services/userService.js
+++ services/userService.js
@@ -7,24 +7,42 @@ const getdata = async () => {
 };
 
 const login = async (email, password) => {
-  let checkEmail = await User.query().where("users_email", email).first();
-  if (checkEmail.password.length === 60) {
-    let checkPass = await bcrypt.compare(password, checkEmail.password);
-    if (checkPass === true) {
-      return { Success: "Login Success" };
-    } else {
-      return { Error: "Invalid Cradensial" };
-    }
-  } else {
-    let loginDate = moment();
-    let expiryDate = checkEmail.password_exp_date;
-    let duration = moment.duration(moment(expiryDate).diff(loginDate));
+  let checkEmail = await User.query()
+    .where("users_email", email)
+    .orWhere("user_name", email)
+    .first();
 
-    if (duration.asDays() > 0) {
-      if (checkEmail.password === password) {
-        return { Success: "Login Success" };
+  try {
+    if (checkEmail) {
+      let loginDate = moment();
+      let expiryDate = checkEmail.password_exp_date;
+      let duration = moment.duration(moment(expiryDate).diff(loginDate));
+
+      if (checkEmail.password.length === 60) {
+        let checkPass = await bcrypt.compare(password, checkEmail.password);
+        if (checkPass === true) {
+          return checkEmail;
+        } else {
+          return { Erorr: "Invalid Credentials" };
+        }
       }
+      if (duration.asDays() > 0) {
+        if (checkEmail.password === password) {
+          return checkEmail;
+        } else {
+          return { Erorr: "Invalid Credentials" };
+        }
+      } else {
+        return { Erorr: "Your Password is Expire" };
+      }
+    } else {
+      return { Erorr: "Invalid Credentials" };
     }
+  } catch (error) {
+    return {
+      message: error.message,
+      stack: error.stack,
+    };
   }
 };
 
